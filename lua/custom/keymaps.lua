b2db208848b223643b68612bd38a3ea6a61540e8
William Gan <williamgan@Williams-MacBook-Air-2.local>
2025-01-15 21:01:46 +0100

    change some keymaps
    change search buffer keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,5 +2,13 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 -- My keysmap
-map('n', '<c-]>', '<cmd>bn<cr>', { silent = true, desc = 'Next buffer' })
-map('n', '<c-[>', '<cmd>bp<cr>', { silent = true, desc = 'Previous buffer' })
+
+-- buffer
+map('n', '<D-]>', ':bn<cr>', opts)
+map('n', '<D-[>', ':bp<cr>', opts)
+map('n', '<c-c>', ':bd<cr>', opts)
+
+-- cmd + s => :w
+map('n', '<D-s>', ':w<CR>', opts)
+map('i', '<D-s>', '<Esc>:w<CR>a', opts)
+map('v', '<D-s>', '<Esc>:w<CR>gv', opts)
