93bc83c7d10a2f8139880198f334ade4c5334cbe
William Gan <7371364+w1ll14m64n@users.noreply.github.com>
2025-01-16 01:27:33 +0100

    More awesome keys

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,3 +12,26 @@ map('n', '<c-c>', ':bd<cr>', opts)
 map('n', '<D-s>', ':w<CR>', opts)
 map('i', '<D-s>', '<Esc>:w<CR>a', opts)
 map('v', '<D-s>', '<Esc>:w<CR>gv', opts)
+
+-- cmd + x => exit input mode, go to visual mode
+map('i', '<D-x>', '<Esc>v', opts)
+
+-- Vertical scroll and center
+map('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+map('n', 'n', 'nzzzv', opts)
+map('n', 'N', 'Nzzzv', opts)
+
+-- Window management
+map('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+map('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+map('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+map('n', '<leader>cs', ':close<CR>', opts) -- close current split window
+
+-- Toggle line wrapping
+map('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+-- Keep last yanked when pasting
+map('v', 'p', '"_dP', opts)
